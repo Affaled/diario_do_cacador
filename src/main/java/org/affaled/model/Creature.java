942510aa3563b558ad4bbe77878f2df0178aa81f
Affaled <affaledl@gmail.com>
2025-12-06 01:01:16 -0300

    alteração na classe de criatura

--- src/main/java/org/affaled/model/Creature.java
+++ src/main/java/org/affaled/model/Creature.java
@@ -4,15 +4,24 @@ public abstract class Creature {
     protected String name;
     protected int currentHp;
     protected int maxHp;
+    protected boolean isAlive;
 
-    public Creature(String name, int maxHp) {
+    protected Creature(String name, int maxHp) {
         this.name = name;
         this.maxHp = maxHp;
         this.currentHp = maxHp;
+        this.isAlive = true;
     }
 
-    public void takeDamage(int damage) {
-        currentHp -= damage;
+    public void takeDamage(int amount) {
+        currentHp -= amount;
         if (this.currentHp < 0) this.currentHp = 0;
     }
+
+    public void heal(int amount) {
+        currentHp -= amount;
+        if (this.currentHp > maxHp) this.currentHp = maxHp;
+    }
+
+    public abstract void attack(Creature target);
 }
